3e9c19b77b1d8f5499aedd905e5b0234599fc80e
Tyler Llorens <tylerllore@gmail.com>
2025-06-06 12:53:32 -0400

    Finished Code

--- db/queries/employees.js
+++ db/queries/employees.js
@@ -1,6 +1,11 @@
+import db from "#db/client";
 /** @returns the employee created according to the provided details */
 export async function createEmployee({ name, birthday, salary }) {
-  // TODO
+  const sql = ` INSERT INTO employees (name, birthday, salary) VALUES ($1, $2, $3) RETURNING *`;
+  const {
+    rows: [employee],
+  } = await db.query(sql, [name, birthday, salary]);
+  return employee;
 }
 
 // === Part 2 ===
@@ -22,9 +27,7 @@ export async function getEmployee(id) {
  * @returns the updated employee with the given id
  * @returns undefined if employee with the given id does not exist
  */
-export async function updateEmployee({ id, name, birthday, salary }) {
-  // TODO
-}
+export async function updateEmployee({ id, name, birthday, salary }) {}
 
 /**
  * @returns the deleted employee with the given id
--- db/schema.sql
+++ db/schema.sql
@@ -1,3 +1,7 @@
 DROP TABLE IF EXISTS employees;
 
+CREATE TABLE employees (
+    id SERIAL PRIMARY KEY, name TEXT NOT NULL, birthday DATE NOT NULL, salary INTEGER NOT NULL
+)
+
 
